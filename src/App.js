4942789eb7f1d24608d7e5af94f9dc2ef6eac828
vovam <vova.myltsin@yandex.ru>
2020-07-06 12:19:33 +0400

    init project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Dashboard from './dashboard/Dashboard'
+
+function App() {
+    return (<div className='main'>
+        <Dashboard/>
+    </div>);
+}
+
+export default App;
